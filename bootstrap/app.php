3bb301ebaa5ea158627f6399e53428359b754cba
Muhammad Asharul Maali <masharul51@gmail.com>
2026-02-26 08:06:42 +0700

    fix: redirect unauthenticated users to cas.login instead of login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,5 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json(['message' => $e->getMessage()], 401);
+            }
+
+            return redirect()->guest(route('cas.login'));
+        });
     })->create();
