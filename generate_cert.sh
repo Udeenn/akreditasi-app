439720811575e9080bfb35cf59fef1a268aed23e
MuhammadAsharul <masharul51@gmail.com>
2026-06-29 11:30:12 +0700

    chore: add deployment scripts and documentation to gitignore

--- generate_cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Generate self-signed SSL certificate untuk development
-# MSYS_NO_PATHCONV=1 mencegah Git Bash mengkonversi path di Windows
-MSYS_NO_PATHCONV=1 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout .docker/nginx/selfsigned.key \
-  -out .docker/nginx/selfsigned.crt \
-  -subj "/C=ID/ST=JawaTengah/L=Surakarta/O=UMS/OU=IT/CN=mam.ums.ac.id"
